ae9e757848842ef5e06f80cbe107a75e611d4062
jacksonspenc <jacksonspencer327@gmail.com>
2025-01-26 20:32:05 -0700

    commit

--- mission/mission.js
+++
@@ -1,9 +0,0 @@
-const elements = document.querySelectorAll(select);
-
-const themeSelector = select
-function changeTheme() {
-    if (themeSelector.value) {
-    
-    }
-}
-themeSelector.addEventListener('change', changeTheme);
