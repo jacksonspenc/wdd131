b2c55a5cfd41e7ccb0aabb449a0f170f2c643c7c
jacksonspenc <jacksonspencer327@gmail.com>
2025-02-07 14:19:07 -0700

    commit 1

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -10,39 +10,45 @@ function adjustMenuVisibility() {
         navItems.style.display = 'flex';
     }
 }
-document.getElementById('menu').addEventListener('click', function() {
-    var navItems = document.querySelector('nav ul');
-    if (navItems.style.display === 'none' || navItems.style.display === '') {
-        navItems.style.display = 'flex';
-    } else {
-        navItems.style.display = 'none';
-    }
-});
-function viewerTemplate(pic, alt) {
-    return `<div class="viewer" style="display:none;">
-      <span class="close-viewer">X</span>
-      <img src="${pic}" alt="${alt}">
-    </div>`;
+document.querySelector("#menu-button").addEventListener("click", show_hide)
+window.addEventListener("resize", handleResize);
+const gallery = document.querySelectorAll("img");
+for (const image of gallery) {
+    image.addEventListener("click", viewHandler)
 }
-function viewHandler(event) {
-    if (event.target.tagName === "IMG") {
-        var clicked = event.target;
-        var imgSrc = clicked.src;
-        var baseName = imgSrc.substring(0, imgSrc.lastIndexOf("-"));
-        var newSrc = `${baseName}-full.jpeg`;
-        var largeImg = viewerTemplate(newSrc, clicked.alt);
 
-        document.body.insertAdjacentHTML("afterbegin", largeImg);
-        document.querySelector(".viewer").style.display = 'flex';
-        document.querySelector(".close-viewer").addEventListener('click', closeViewer);
+function show_hide() {
+    const navigation = document.querySelector(".navigation");
+    navigation.classList.toggle("hide");
+}
+
+function handleResize() {
+    const width = window.innerWidth;
+    const navigation = document.querySelector(".navigation");
+    if (width > 1000) {
+        navigation.classList.remove("hide");
+    } else {
+        navigation.classList.add("hide")
     }
 }
-document.querySelector('.gallery').addEventListener('click', viewHandler);
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${pic}" alt="${alt}">
+        </div>`;
+}
+
+function viewHandler(event) {
+    const element = event.target;
+    const srcValue = element.getAttribute("src");
+    const words = srcValue.split("-");
+    const imageFileName = words[0] + "-full.jpeg";
+    console.log(imageFileName);
+    document.querySelector("body").insertAdjacentHTML("afterbegin", viewerTemplate(imageFileName, "image"));
+    document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+}
+
 function closeViewer() {
-    const viewer = document.querySelector('.viewer');
-    if (viewer) {
-        viewer.remove();
-    }
+    document.querySelector("body").removeChild(document.querySelector("div"));
 }
-window.addEventListener('resize', adjustMenuVisibility);
-adjustMenuVisibility();
