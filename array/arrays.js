f4e0802ffee8b8486393145008aa01df732870b7
jacksonspenc <jacksonspencer327@gmail.com>
2025-02-20 16:21:48 -0700

    commit 1

---
+++ array/arrays.js
@@ -0,0 +1,25 @@
+// example 2
+const grades = ["A", "B", "A"];
+function convertGradeToPoints(grade) {
+  let points = 0;
+  if (grade === "A") {
+    points = 4;
+  } else if (grade === "B") {
+    points = 3;
+  }
+  return points;
+}
+const gpaPoints = grades.map(convertGradeToPoints);
+const gpaPoints = grades.map(convertGradeToPoints);
+const pointsTotal = gpaPoints.reduce(function (total, item) {
+  return total + item;
+});
+const gpa = pointsTotal / gpaPoints.length;
+
+// example 2
+// this is the same thing as above, but with an arrow function
+const pointsTotal = gpaPoints.reduce((total, item) => total + item);
+const gpa = pointsTotal / gpaPoints.length;
+
+// this could be further simplified as
+const gpa = gpaPoints.reduce((total, item) => total + item) / gpaPoints.length;
