9741280f972ea0f44dd4b5b0b1c8323eb55ebe75
jacksonspenc <jacksonspencer327@gmail.com>
2025-03-25 11:50:56 -0600

    commit 2

--- recipes/main.js
+++ recipes/main.js
@@ -1,4 +1,4 @@
-import recipes from "./recipes.mjs"
+import recipes from ".recipes.mjs"
 
 function random(num) {
     return Math.floor(Math.random()*num);
@@ -7,3 +7,10 @@ function random(num) {
 function getRandomListEntry(list) {
     return list[random(list.length)];
 }
+
+function init() {
+    // get a random recipe
+    const recipe = getRandomListEntry(recipes);
+    // render the recipe with renderRecipes.
+    renderRecipes([recipe]);
+  }
