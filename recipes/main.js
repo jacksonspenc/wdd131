8dd5a4ab01aa0ef9a3d2a7361b04002c12a95daf
jacksonspenc <jacksonspencer327@gmail.com>
2025-03-25 11:58:12 -0600

    commit 4

--- recipes/main.js
+++ recipes/main.js
@@ -1,7 +1,7 @@
 import recipes from "./recipes.mjs"
 
 function random(num) {
-    return Math.floor(Math.random()*5);
+    return Math.floor(Math.random()*num);
 }
 
 function getRandomListEntry(list) {
@@ -11,7 +11,38 @@ function getRandomListEntry(list) {
 function init() {
     // get a random recipe
     const recipe = getRandomListEntry(recipes);
-    // render the recipe with renderRecipes.
     renderRecipes([recipe]);
 }
 
+function tagsTemplate(tags) {
+    var html = ``;
+    html += `<div class="tags-container">`;
+    tags.forEach((tag) => {
+        html += `<h3 class="category">${tag}</h3>`;
+    });
+    html += `</div>`;
+	return html;
+}
+
+function ratingTemplate(rating) {
+	let html = `
+        <span
+            class="rating"
+            role="img"
+            aria-label="Rating: ${rating} out of 5 stars"
+        >
+    `;
+    for (let i = 1; i <= 5; i++) {
+        if (i <= rating) {
+            html += `
+                <span aria-hidden="true" class="icon-star">⭐</span>
+            `;
+        } else { // else output an empty star
+            html += `
+                <span aria-hidden="true" class="icon-star-empty">☆</span>
+            `;
+        }
+    }
+	html += `</span>`;
+	return html;
+}
